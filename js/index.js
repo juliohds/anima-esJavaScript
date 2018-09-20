e037ddd6c831e588ec5ad79641d84ae8a07954ba
juliohds <tecjuliohenrique@gmail.com>
2018-09-19 22:45:06 -0300

    estrutura algoritmo 1

--- js/index.js
+++ js/index.js
@@ -1,10 +1,18 @@
 $(document).ready(function() {
+  
+  url = 'http://localhost:8080/escalonamento/algoritmo?id=';
+  
+  txtAlgoritmo = "";
+  elementosProcesso = [];
+
+  carregaAlgoritmo(1);
   var left = 0;
+
   var p1 = true;
 
   $(document).on("keyup", function(e) {
     var key = e.which;
-
+    
     if (key == 39) {
       // the enter key ascii code
       left += 150;
@@ -42,3 +50,43 @@ $(document).ready(function() {
     }
   });
 });
+
+function arrayDeObjectosPessoa(pessoas){
+  var array = [];
+  aux = 0;
+  pessoas.forEach(element => {
+    obj = {
+      img: element.img,
+    }
+    array[aux] = obj;
+    aux++;
+  });
+  return array;
+}
+
+function carregaAlgoritmo(id){
+  axios.get(url+id)
+  .then(function (response) {
+    
+    var data = response.data;
+    //set variaveis
+    console.log(response.data);
+    txtAlgoritmo  = data.nome;
+    elementosProcesso = arrayDeObjectosPessoa(data.pessoa);
+    atualizaCampos();
+  });
+}
+
+function atualizaCampos(){
+  $("#txtAlgoritmo").text('Algoritmo: '+txtAlgoritmo);
+  montaDivInicio();
+}
+
+function montaDivInicio(){
+  elementosProcesso.forEach(processo => {
+    $("#divInicio").append(
+      '<img src="https://img.ashampoo.com/ashampoo.com_images/img/1/products/1304/en/box_ashampoo_spectre-meltdown-cpu-checker_800x800.png" alt="" width="90" height="90" style="margin: 10px;">'
+    );
+  });
+  
+}
